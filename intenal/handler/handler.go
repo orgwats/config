654ed493b850fc575a0eac4c6723c840191f8890
waik1ki <arbutus@outlook.kr>
2025-04-24 16:08:55 +0900

    feat: 서비스 핸들러 추가

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -1,1 +1,12 @@
 package handler
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/orgwats/config/intenal/service"
+)
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return service.GetConfig(req)
+}
