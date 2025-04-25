8751069de0be4e898b76311ffad76571ad060b40
waik1ki <arbutus@outlook.kr>
2025-04-25 16:31:54 +0900

    test: logging 코드 추가

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -9,6 +9,18 @@ import (
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Println(req)
+	log.Println("req.HTTPMethod : ", req.HTTPMethod)
+	log.Println("req.Headers : ", req.Headers)
+	log.Println("req.Body : ", req.Body)
+	log.Println("req.IsBase64Encoded : ", req.IsBase64Encoded)
+	log.Println("req.MultiValueHeaders : ", req.MultiValueHeaders)
+	log.Println("req.MultiValueQueryStringParameters : ", req.MultiValueQueryStringParameters)
+	log.Println("req.Path : ", req.Path)
+	log.Println("req.PathParameters : ", req.PathParameters)
+	log.Println("req.QueryStringParameters : ", req.QueryStringParameters)
+	log.Println("req.RequestContext : ", req.RequestContext)
+	log.Println("req.Resource : ", req.Resource)
+	log.Println("req.StageVariables : ", req.StageVariables)
+	log.Println("req.RequestContext : ", req.RequestContext)
 	return service.GetConfig(req)
 }
