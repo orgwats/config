28d4d23185b6c4553548a020ef93187329b4432d
waik1ki <arbutus@outlook.kr>
2025-04-25 11:34:47 +0900

    chore: 람다 함수 호출 엔드포인트 예외처리

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -2,11 +2,20 @@ package handler
 
 import (
 	"context"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/orgwats/config/intenal/service"
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return service.GetConfig(req)
+	switch {
+	case strings.HasPrefix(req.Path, "/config"):
+		return service.GetConfig(req)
+	default:
+		return events.APIGatewayProxyResponse{
+			StatusCode: 404,
+			Body:       "Not Found",
+		}, nil
+	}
 }
