511364623ada32d8d4a594a5533b236c2c0daac1
waik1ki <arbutus@outlook.kr>
2025-04-25 16:26:11 +0900

    test: logging 코드 추가

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"context"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/orgwats/config/intenal/service"
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println(req)
 	return service.GetConfig(req)
 }
