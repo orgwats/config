8f0927a3bbd45371cae7b79cc5c18008d67e284c
waik1ki <arbutus@outlook.kr>
2025-04-25 11:45:58 +0900

    test: 엔드포인트 제거

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -2,20 +2,11 @@ package handler
 
 import (
 	"context"
-	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/orgwats/config/intenal/service"
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	switch {
-	case strings.HasPrefix(req.Path, "/config"):
-		return service.GetConfig(req)
-	default:
-		return events.APIGatewayProxyResponse{
-			StatusCode: 404,
-			Body:       "Not Found",
-		}, nil
-	}
+	return service.GetConfig(req)
 }
