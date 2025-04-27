a1219b8ce44c50802980b36fd4e415ae0b42da64
waik1ki <arbutus@outlook.kr>
2025-04-27 14:41:13 +0900

    chore: path 분기처리 추가

--- intenal/handler/handler.go
+++ intenal/handler/handler.go
@@ -2,19 +2,15 @@ package handler
 
 import (
 	"context"
-	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/orgwats/config/intenal/service"
 )
 
 func Handler(ctx context.Context, req events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
-	log.Println("req.Body : ", req.Body)
-	log.Println("req.Cookies : ", req.Cookies)
-	log.Println("req.Headers : ", req.Headers)
-	log.Println("req.QueryStringParameters : ", req.QueryStringParameters)
-	log.Println("req.RawPath : ", req.RawPath)
-	log.Println("req.RawQueryString : ", req.RawQueryString)
-	log.Println("req.RequestContext : ", req.RequestContext)
-	return service.GetConfig(req)
+	if req.RawPath != "/config" {
+		return events.LambdaFunctionURLResponse{StatusCode: 404, Body: "Not Found"}, nil
+	} else {
+		return service.GetConfig(req)
+	}
 }
